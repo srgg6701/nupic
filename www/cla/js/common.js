aad88b3a7e33e9aa22838ad4ae5c0ab8681050fc
srgg6701 <srgg67@gmail.com>
2014-04-30 23:52:31 +0400

    First attempt of the new approach: good enough.

--- www/cla/js/common.js
+++ www/cla/js/common.js
@@ -10,25 +10,53 @@ $(function() {
     });
     // show inhibition area
     $('[data-col-num]').on('mouseenter', function(){
-        var colNum = $(this).attr('data-col-num');
+        var currentColumnNumber = $(this).attr('data-col-num');
 
     });
     $('[data-col-num] div').on('click', function(){
         var colRect = $(this).parent();
         $(colRect).toggleClass('borderGrey');
-        var colNum = parseInt($(colRect).attr('data-col-num'));
-        // calculate its number in the row
-        //var rows = Matrix.columnsSets[Matrix.settings.columns][0];
-        // cols in Matrix
-        var cols = Matrix.columnsSets[Matrix.settings.columns][1];
         //
-        var numberInRow = colNum%cols; //console.log('numberInRow = '+numberInRow);
+        var currentColumnNumber = parseInt($(colRect).attr('data-col-num'));
+        // calculate its number in the row
+        var rows = Matrix.columnsSets[Matrix.settings.columns][0];
+        // columnsInMatrix in Matrix
+        var columnsInMatrix = Matrix.columnsSets[Matrix.settings.columns][1];
+        //
+        var numberInRow = currentColumnNumber%columnsInMatrix; //console.log('numberInRow = '+numberInRow);
         var inhRadius = Matrix.settings.inhibition_radius[0];
         // calculate the rows above the column
-        var rows_above = (colNum-numberInRow)/cols,
+        var rows_above = (currentColumnNumber-numberInRow)/columnsInMatrix,
+            //              107 - 8 / 11
+            rows_bellow = rows_above+inhRadius+ 1,
             recHorizontalLen = inhRadius*2;
 
+        var leftEdge=null,
+            topEdge=null,
+            rightEdge=null,
+            bottomEdge=null;
 
+        if((currentColumnNumber%columnsInMatrix==0)
+            || currentColumnNumber%columnsInMatrix>inhRadius)
+            leftEdge = true;
+
+        if(rows_above>=inhRadius)
+            topEdge = true;
+
+        if((currentColumnNumber%columnsInMatrix>0)
+            && currentColumnNumber%columnsInMatrix<=recHorizontalLen+2)
+            rightEdge = true;
+
+        if(rows_bellow<=rows)
+            bottomEdge = true;
+            
+        console.log('leftEdge: '+leftEdge+'\ntopEdge: '+topEdge+'\nrightEdge: '+rightEdge+'\nbottomEdge: '+bottomEdge);
+
+        var topLeft=null,
+            topRight=null,
+            bottomLeft=null,
+            bottomRight=null;
+        
 
     });
 
